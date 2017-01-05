e2c71b273167f9302c3919e6ce658991d686e1a0
Alberto Silva <albertossilva@gmail.com>
2017-01-05 02:15:58 -0200

    Adding default to avoid breaks

--- Infra/api/graphQL/login/authenticate.mutation.js
+++ Infra/api/graphQL/login/authenticate.mutation.js
@@ -15,7 +15,7 @@ module.exports = {
   resolve: (_, { username, password }, { siguex }) => {
     return siguex
       .login(username, password)
-      .then(({ success, token }) => {
+      .then(({ success, token } = {}) => {
         if (success) {
           siguex.setToken(token)
           return 'OK'
